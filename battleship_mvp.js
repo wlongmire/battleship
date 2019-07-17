728722307a22cc3e8b28de015b46422e690365e9
Warren Longmire <warrenlongmire@gmail.com>
2019-07-17 08:37:10 -0400

    initial commit

---
+++ battleship_mvp.js
@@ -0,0 +1,38 @@
+/*
+Pseudo code:
+
+Main goals:
+1. Create a 10x10 grid
+2. Create ships, that the user can't see in random positions
+  - Ships can just be rectangles of diffierent sizes, it's just an mvp
+3. Check if the user clicks on one of the 'ships' if they do, change the color and reveal it to the user
+4. Let user know when they got all the ships
+
+Extra credit:
+1. Create a way to reset the board
+2. Track the users score
+  - Number of hits
+  - Number of ships remaining
+
+
+Other:
+1. Mark where the user has hit
+2. Maybe get some sort of wave effect for the background
+
+Example future goals (Things that you'd do after you create a working MVP, don't worry about them for this though it's an MVP project)
+1. Stop any overlapping
+2. Random Orientation
+3. Graphics for ships and missles
+4. Visual effects
+5. Moving ocean background
+etc...
+*/
+
+function setup() {
+}
+
+function draw() {
+}
+
+function mousePressed() {
+}
